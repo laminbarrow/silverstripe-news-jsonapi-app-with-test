529b17290a5498dc6e4130ca01c2ddfc42d8315e
Lamin Barrow <laminbarrow@gmail.com>
2018-06-21 17:11:38 +0000

    Added more test. Create default records only in dev or test modes

--- mysite/code/DataObjects/Article.php
+++ mysite/code/DataObjects/Article.php
@@ -3,6 +3,7 @@
 namespace mysite\DataObjects;
 
 use Faker\Factory;
+use SilverStripe\Control\Director;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DB;
 
@@ -38,20 +39,22 @@ class Article extends DataObject
     public function requireDefaultRecords()
     {
         //let us create our selfs a few news items when running this in dev mode
-        //when no Articles exists
-        if (!self::get()->first()) {
+        //when no Articles exists in dev or test mode
+        if (Director::isDev() || Director::isTest()) {
 
-            $newsItesmToCreate = 5;
-            $faker = Factory::create();
+            if (!self::get()->first()) {
 
-            for ($i = 1; $i <= $newsItesmToCreate; $i++) {
-                $article = new self();
-                $article->Title = $faker->sentence;
-                $article->Content = $faker->text(200);
-                $article->write();
-                DB::alteration_message('New Article created', 'created');
+                $newsItesmToCreate = 5;
+                $faker = Factory::create();
+
+                for ($i = 1; $i <= $newsItesmToCreate; $i++) {
+                    $article = new self();
+                    $article->Title = $faker->sentence;
+                    $article->Content = $faker->text(200);
+                    $article->write();
+                    DB::alteration_message('New Article created', 'created');
+                }
             }
         }
-
     }
 }
--- mysite/tests/functional/APIControllerTest.php
+++ mysite/tests/functional/APIControllerTest.php
@@ -1,12 +1,12 @@
 <?php
 
 use mysite\APIController;
-use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Dev\FunctionalTest;
 
 /**
  * API Test Controller
  */
-class APIControllerTest extends SapphireTest
+class APIControllerTest extends FunctionalTest
 {
     /**
      * Path to fixture data for this test run.
@@ -17,6 +17,15 @@ class APIControllerTest extends SapphireTest
      * @var string|array
      */
     protected static $fixture_file = null;
+
+    /**
+     * @var Boolean If set to TRUE, this will force a test database to be generated
+     * in {@link setUp()}. Note that this flag is overruled by the presence of a
+     * {@link $fixture_file}, which always forces a database build.
+     *
+     * @var bool
+     */
+    protected $usesDatabase = null;
 
     /**
      * Setup
@@ -45,10 +54,43 @@ class APIControllerTest extends SapphireTest
     /**
      * Verify that the APIController class exists
      *
-     * @return void
      */
     public function testAPIControllerClassExist()
     {
-        $this->assertInstanceOf(APIController::class, APIController::create());
+        $this->assertInstanceOf(
+            APIController::class,
+            APIController::create()
+        );
+    }
+
+    /**
+     * api/
+     * API index route
+     */
+    public function testIndexRoute()
+    {
+        $apiRoute = $this->get('api/');
+
+        //api route should load with status 200
+        $this->assertEquals(200, $apiRoute->getStatusCode());
+
+        //make sure that the response is json
+        $this->assertJson($apiRoute->getBody());
+    }
+
+    /**
+     * api/articles route
+     *
+     */
+    public function testArticlesRoute()
+    {
+        $apiArticlesRoute = $this->get("api/articles");
+
+        //make sure that the api/articles route
+        //returns a 200 response code
+        $this->assertEquals(200, $apiArticlesRoute->getStatusCode());
+
+        //make sure that the response is json
+        $this->assertJson($apiArticlesRoute->getBody());
     }
 }
